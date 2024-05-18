c9829f37fcab68509aa1f108df1bc1c189f70b55
IbrahimNimer <ibrahimnimer25@gmail.com>
2024-05-18 18:26:40 +0000

    Update Search

--- src/Component/Search/Search.js
+++ src/Component/Search/Search.js
@@ -1,107 +1,6 @@
-// import React, { useState, useEffect } from 'react';
-// import axios from 'axios';
-
-// function Search() {
-//     const [query, setQuery] = useState('');
-//     const [results, setResults] = useState([]);
-//     const [loading, setLoading] = useState(false);
-//     const [error, setError] = useState(null);
-//     const [detailedResults, setDetailedResults] = useState([]);
-
-//     useEffect(() => {
-//         if (query) {
-//             const fetchData = async () => {
-//                 setLoading(true);
-//                 try {
-//                     const response = await axios.get('http://localhost:3001/auto-complete', {
-//                         params: {
-//                             query,
-//                             hitsPerPage: 3,
-//                             page: 1,
-//                             lang: 'en'
-//                         }
-//                     });
-//                     setResults(response.data.hits); // Adjust based on your response structure
-//                 } catch (error) {
-//                     setError('An error occurred while fetching data');
-//                 }
-//                 setLoading(false);
-//             };
-
-//             fetchData();
-//         }
-//     }, [query]);
-
-//     useEffect(() => {
-//         const fetchDetails = async () => {
-//             const externalIDs = results.map(result => result.externalID).join(',');
-//             try {
-//                 const listResponse = await axios.get('http://localhost:3001/properties/list', {
-//                     params: {
-//                         locationExternalIDs: externalIDs,
-//                         purpose: 'for-sale', // or 'rent', adjust as needed
-//                         hitsPerPage: 3 // or any other value you want
-//                     }
-//                 });
-//                 if (Array.isArray(listResponse.data.hits)) {
-//                     const detailedResultsPromises = listResponse.data.hits.map(async (property) => {
-//                         try {
-//                             const response = await axios.get('http://localhost:3001/properties/detail', {
-//                                 params: { externalID: property.externalID }
-//                             });
-//                             return response.data;
-//                         } catch (error) {
-//                             console.error('Error fetching property details:', error);
-//                             return null;
-//                         }
-//                     });
-//                     const detailedResults = await Promise.all(detailedResultsPromises);
-//                     setDetailedResults(detailedResults.filter(result => result !== null));
-//                 } else {
-//                     console.error('Invalid response format for property list:', listResponse.data);
-//                 }
-//             } catch (error) {
-//                 console.error('Error fetching property list:', error);
-//             }
-//         };
-
-//         if (results.length > 0) {
-//             fetchDetails();
-//         }
-//     }, [results]);
-
-//     const handleInputChange = (e) => {
-//         setQuery(e.target.value);
-//     };
-
-//     return (
-//         <div>
-//             <input
-//                 type="text"
-//                 value={query}
-//                 onChange={handleInputChange}
-//                 placeholder="Search..."
-//             />
-//             {loading && <p>Loading...</p>}
-//             {error && <p>{error}</p>}
-//             <ul>
-//                 {detailedResults.map((detail, index) => (
-//                     <li key={index}>
-//                         <h3>{detail.title}</h3>
-//                         <img src={detail.coverPhoto.url} alt="Cover" />
-//                         <p><strong>Purpose:</strong> {detail.purpose}</p>
-//                         <p>{detail.description}</p>
-//                     </li>
-//                 ))}
-
-//             </ul>
-//         </div>
-//     );
-// }
-
-// export default Search;
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import './Search.css';
 
 function Search() {
     const [query, setQuery] = useState('');
@@ -112,13 +11,11 @@ function Search() {
     const [initialResults, setInitialResults] = useState([]);
     const [initialDetailedResults, setInitialDetailedResults] = useState([]);
 
-    // Function to strip HTML tags from a string
     const stripHTML = (html) => {
         const doc = new DOMParser().parseFromString(html, 'text/html');
         return doc.body.textContent || "";
     };
 
-    // Fetch random data on initial load
     useEffect(() => {
         const fetchInitialData = async () => {
             setLoading(true);
@@ -130,7 +27,7 @@ function Search() {
                         hitsPerPage: 6,
                     },
                 });
-                setInitialResults(response.data.hits); // Adjust based on your response structure
+                setInitialResults(response.data.hits);
                 const initialIDs = response.data.hits.map(hit => hit.externalID);
                 fetchInitialDetails(initialIDs);
             } catch (error) {
@@ -162,7 +59,6 @@ function Search() {
         fetchInitialData();
     }, []);
 
-    // Fetch search results
     const fetchSearchResults = async () => {
         setLoading(true);
         try {
@@ -174,27 +70,35 @@ function Search() {
                     lang: 'en'
                 }
             });
-            setResults(response.data.hits); // Adjust based on your response structure
+            setResults(response.data.hits);
         } catch (error) {
             setError('An error occurred while fetching data');
         }
         setLoading(false);
     };
 
-    // Fetch detailed results based on search results
     useEffect(() => {
         const fetchDetails = async () => {
             const externalIDs = results.map(result => result.externalID).join(',');
             try {
-                const listResponse = await axios.get('http://localhost:3001/properties/list', {
+                const saleResponse = await axios.get('http://localhost:3001/properties/list', {
                     params: {
                         locationExternalIDs: externalIDs,
-                        purpose: 'for-sale', // or 'rent', adjust as needed
-                        hitsPerPage: 3 // or any other value you want
+                        purpose: 'for-sale',
+                        hitsPerPage: 3
                     }
                 });
-                if (Array.isArray(listResponse.data.hits)) {
-                    const detailedResultsPromises = listResponse.data.hits.map(async (property) => {
+
+                const rentResponse = await axios.get('http://localhost:3001/properties/list', {
+                    params: {
+                        locationExternalIDs: externalIDs,
+                        purpose: 'for-rent',
+                        hitsPerPage: 3
+                    }
+                });
+
+                if (Array.isArray(saleResponse.data.hits) && Array.isArray(rentResponse.data.hits)) {
+                    const saleDetailedResultsPromises = saleResponse.data.hits.map(async (property) => {
                         try {
                             const response = await axios.get('http://localhost:3001/properties/detail', {
                                 params: { externalID: property.externalID }
@@ -205,10 +109,25 @@ function Search() {
                             return null;
                         }
                     });
-                    const detailedResults = await Promise.all(detailedResultsPromises);
-                    setDetailedResults(detailedResults.filter(result => result !== null));
+
+                    const rentDetailedResultsPromises = rentResponse.data.hits.map(async (property) => {
+                        try {
+                            const response = await axios.get('http://localhost:3001/properties/detail', {
+                                params: { externalID: property.externalID }
+                            });
+                            return response.data;
+                        } catch (error) {
+                            console.error('Error fetching property details:', error);
+                            return null;
+                        }
+                    });
+
+                    const saleDetailedResults = await Promise.all(saleDetailedResultsPromises);
+                    const rentDetailedResults = await Promise.all(rentDetailedResultsPromises);
+
+                    setDetailedResults([...saleDetailedResults.filter(result => result !== null), ...rentDetailedResults.filter(result => result !== null)]);
                 } else {
-                    console.error('Invalid response format for property list:', listResponse.data);
+                    console.error('Invalid response format for property list:', saleResponse.data, rentResponse.data);
                 }
             } catch (error) {
                 console.error('Error fetching property list:', error);
@@ -231,23 +150,27 @@ function Search() {
     };
 
     return (
-        <div>
-            <input
-                type="text"
-                value={query}
-                onChange={handleInputChange}
-                placeholder="Search..."
+        <div className="search-container">
+            <input 
+                className="search-input" 
+                type="text" 
+                value={query} 
+                onChange={handleInputChange} 
+                placeholder="Search..." 
             />
-            <button onClick={handleSearch}>Search</button>
-            {loading && <p>Loading...</p>}
-            {error && <p>{error}</p>}
-            <ul>
+            <button className="search-button" onClick={handleSearch}>Search</button>
+            {loading && <p className="loading-message">Loading...</p>}
+            {error && <p className="error-message">{error}</p>}
+            <ul className="results-list">
                 {(detailedResults.length > 0 ? detailedResults : initialDetailedResults).map((detail, index) => (
-                    <li key={index}>
+                    <li className="result-item" key={index}>
                         <h3>{stripHTML(detail.title)}</h3>
                         <img src={detail.coverPhoto.url} alt="Cover" />
+                        <p><strong>City:</strong> {detail.location.map(loc => loc.name).join(', ')}</p>
                         <p><strong>Purpose:</strong> {detail.purpose}</p>
-                        <p>{stripHTML(detail.description)}</p>
+                        <p><strong>Type:</strong> {detail.category[0].name}</p>
+                        <p><strong>Price:</strong> {detail.price}</p>
+                        <p><strong>Description:</strong> {stripHTML(detail.description)}</p>
                     </li>
                 ))}
             </ul>
@@ -256,3 +179,4 @@ function Search() {
 }
 
 export default Search;
+
