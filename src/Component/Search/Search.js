0dd56d721f3384638f5695c4efa3b7f6353a1b96
IbrahimNimer <ibrahimnimer25@gmail.com>
2024-05-19 18:49:22 +0000

    Modal Editing

--- src/Component/Search/Search.js
+++ src/Component/Search/Search.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
-import millify from 'millify'; // Import millify
+import millify from 'millify'; 
 import './Search.css';
 
 function Search() {
